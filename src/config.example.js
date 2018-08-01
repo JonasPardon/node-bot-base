54ac50fb3ddfeaf4335bb7f4cbfcafc8ef0a07b4
Jonas Pardon <jonaspardon@hotmail.com>
2018-08-01 15:02:22 +0200

    added example config

---
+++ src/config.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    // If true, commands can only be called by the bot owners
+    dev: false,
+
+    token: '',
+    // Array of user ID's
+    owner: [
+        ''
+    ],
+    prefix: '!',
+
+}
